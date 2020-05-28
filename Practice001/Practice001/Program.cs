e07a7e7a77bb58906495668994f102a2c173ecc8
Wei Xiaoyu <wxy_404@github.com>
2020-05-29 06:25:12 +0900

    list配列

---
+++ Practice001/Practice001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new List<string>();
+            str.Add("abc");
+            str.Add("sss");
+
+            Console.WriteLine(str[0]);
+            Console.WriteLine(str[1]);
+        }
+    }
+}
